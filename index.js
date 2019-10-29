14091546293ce50634ef12d4765327cc3e3a7ae5
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-10-28 21:24:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//requiring the team.js class
+const Team = require("./team.js");
+//requiring the filesystem module
+const fs = require("fs");
+//creating and storing a new team object
+const team = new Team("NewProject");
+
